51533c185fd84e6ab02d3a97f2bccfd378cfe1b4
sebastian-triana <striana96@misena.edu.co>
2021-02-01 09:43:17 -0500

    Chore: se actualiza codigo

--- app/Http/Controllers/AdminfichaController.php
+++ app/Http/Controllers/AdminfichaController.php
@@ -18,12 +18,28 @@ class AdminfichaController extends Controller
     public function create(){
         return view ( 'adminficha.create');
     }
+// registrar nueva ficha
+    public function store(Request $request){
+        $adminfichas=Adminficha::create($request->all());
+        return redirect()->route('adminficha.index')->with([
+            'message'=>'Se ha creado correctamente la ficha',
+            'type'=>'success'
+        ]);
+    }
+
+    //Actualizar una ficha
+    public function update(Request $request, $id){
+        $adminfichas=Adminficha::find($id)->update($request->all());
+        return redirect()->route('adminficha.index')->with([
+            'message'=>'Se ha actualizado correctamente la ficha',
+            'type'=>'warning'
+        ]);
+    }
 
     public function deactivate(Request $request){
         $adminfichas=Adminficha::request($request->all());
     }
 
-    public function
 
 
 
