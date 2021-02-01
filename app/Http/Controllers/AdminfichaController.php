109944ab9567f209d8583f5953456f39e1b45cc4
sebastian-triana <striana96@misena.edu.co>
2021-02-01 09:26:27 -0500

    Chore: se crea crud de Adminficha

--- app/Http/Controllers/AdminfichaController.php
+++ app/Http/Controllers/AdminfichaController.php
@@ -9,4 +9,23 @@ use App\Models\Adminficha;
 class AdminfichaController extends Controller
 {
     //
+    public function index(){
+
+        $adminfichas=Adminficha::all();
+        return view('admindficha.index');
+    }
+
+    public function create(){
+        return view ( 'adminficha.create');
+    }
+
+    public function deactivate(Request $request){
+        $adminfichas=Adminficha::request($request->all());
+    }
+
+    public function
+
+
+
+
 }
