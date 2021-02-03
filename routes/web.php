3813feb463dbcbed4f5056db20778f19d181e348
sebastian-triana <striana96@misena.edu.co>
2021-02-03 15:27:23 -0500

    Chore: se actualiza codigo

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminfichaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('company',[AdminfichaController::class,'index'])->name('.index');
+Route::get('company/create',[AdminfichaController::class,'create'])->name('adminficha.create');
+Route::post('company',[AdminfichaController::class,'store'])->name('adminficha.store');
+Route::get('company/{id}',[AdminfichaController::class,'show'])->name('adminficha.find');
+Route::get('company/edit/{id}',[AdminfichaController::class,'edit'])->name('adminficha.edit');
+Route::put('company/{id}',[AdminfichaController::class,'update'])->name('adminficha.update');
+Route::get('company/deactivate/{id}',[AdminfichaController::class,'deactivate'])->name('adminficha.deactivate');
